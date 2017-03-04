2fd00ee1d7c71aa9ec012df16c3b281218ffc808
axolotl <andre.charles.lewin@gmail.com>
2017-03-04 15:03:38 +0100

    preparis la aspekton per Easy Table Filter

---
+++ display_streams.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    
+    $.getJSON("./twitchsampleAPI.json", function(json) {
+        var html="";
+
+        // For each stream, recreate div depending on the value of "stream"
+
+        // $("#stream-table-body").html(html);
+    });
+    
+});
