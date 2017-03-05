c3dadd0ce4a6193454f686dab9edf5bd97095e03
axolotl <andre.charles.lewin@gmail.com>
2017-03-05 21:04:08 +0100

    akirigis la datumojn de la .json

--- display_streams.js
+++ display_streams.js
@@ -3,8 +3,37 @@ $(document).ready(function () {
     $.getJSON("./twitchsampleAPI.json", function(json) {
         var html="";
 
-        // For each stream, recreate div depending on the value of "stream"
+        // For each stream acquired
+        for (var i = 0; i < json.length; i++){
 
+            var onlineStatus;
+            var description;
+            var name;
+            var url = "#";
+            var logo = "#" ; // Default logo when the stream is offline or does not exist
+
+            // Get the online status
+            if (json[i].hasOwnProperty("stream")) {
+                if (json[i].stream === null) {
+                    onlineStatus = false;
+                    description = "This stream is offline";
+                    name = json[i].display_name;
+                } else {
+                    onlineStatus = true;
+                    description = json[i].stream.status;
+                    name = json[i].stream.name;
+                    url = json[i].stream.url;
+                    logo = json[i].stream.logo;
+                }
+            } else {
+                onlineStatus = false;
+                description = json[i].message;
+                name = json[i].status;
+            }
+        }
+
+        // TODO : Build the html with the properties
+        
         // $("#stream-table-body").html(html);
     });
     
