c9f8dbde99a013a199914ece00d2af3aae982b2b
axolotl <andre.charles.lewin@gmail.com>
2017-04-30 00:17:49 +0200

    implemented the use of the API from Wind Bow

--- display_streams.js
+++ display_streams.js
@@ -1,59 +1,89 @@
+// Store the getted streams in an array of Streams
+var streamList = [];
+var Stream = class {
+    constructor (onlineStatus, description, name, url, logo) {
+        this.onlineStatus = onlineStatus;
+        this.description = description;
+        this.name = name;
+        this.url = url;
+        this.logo = logo;
+    }
+};
+
+
+
 $(document).ready(function () {
-    
-    $.getJSON("./twitchsampleAPI.json", function(json) {
-        var html="";
 
-        // For each stream acquired
-        for (var i = 0; i < json.length; i++){
+    // The results of each stream will be pushed to "json"
+    var followedStreamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "neEkzistas"];
 
-            var onlineStatus;
-            var description;
-            var name;
-            var url = "#";
-            var logo = "#";
-
-            // Get the online status
-            if (json[i].hasOwnProperty("stream")) {
-                if (json[i].stream === null) {
-                    onlineStatus = "offline";
-                    description = "This stream is offline";
-                    name = json[i].display_name;
+    // Get the JSON
+    $.each(followedStreamers, function(i, stream){
+        $.ajax( {
+            dataType: 'jsonp',
+            url: 'https://wind-bow.glitch.me/twitch-api/users/'+stream,
+            type: 'GET',
+            success: function(json) {
+                // If the stream exists, check with a GET request if it is online
+                // Transfer the data about the stream ; because the other does not retrieve these info
+                if (json.hasOwnProperty("display_name")) {
+                    existingStream(stream, json.status, json.name, json.url, json.logo);
                 } else {
-                    onlineStatus = "online";
-                    description = json[i].stream.status;
-                    name = json[i].stream.name;
-                    url = json[i].stream.url;
-                    logo = json[i].stream.logo;
+                    streamList.push(new Stream("offline", json.message, json.status, "#", "#"));
                 }
-            } else {
-                onlineStatus = "offline";
-                description = json[i].message;
-                name = json[i].status;
+
+                buildHTML();
             }
-
-            // Build the html
-            html +=
-                '<tr data-status="' + onlineStatus + '">' +
-                    '<td>' +
-                        '<a href=' + url + '>' +
-                            '<div class="media">' +
-                                '<div class="pull-left">' +
-                                    '<img src="' + logo + '" class="media-photo">' +
-                                '</div>' +
-                                '<div>' +
-                                    '<h4 class="title">' +
-                                        name +
-                                        '<span class="pull-right ' + onlineStatus + '">' + onlineStatus + '</span>' +
-                                    '</h4>' +
-                                    '<p class="summary">' + description + '</p>' +
-                                '</div>' +
-                            '</div>' +
-                        '</a>' +
-                    '</td>' +
-                '</tr>';
-        }
-
-        $("#stream-table-body").html(html);
+        });
     });
-    
 });
+
+
+// If a stream exists, check if it is online (property stream not null), then build the Stream object
+function existingStream(stream, description, name, url, logo){
+    $.ajax( {
+        dataType: 'jsonp',
+        url: 'https://wind-bow.glitch.me/twitch-api/streams/'+stream,
+        type: 'GET',
+        success: function(streamJSON) {
+
+            if (streamJSON.stream != null) {
+                console.log(streamJSON);
+                streamList.push(new Stream("online", streamJSON.stream.channel.status, name, url, logo));
+            } else {
+                streamList.push(new Stream("offline", "This stream is offline", name, url, logo));
+            }
+            buildHTML();
+        }
+    });
+}
+
+
+// Build the displayed html from the Stream objects
+function buildHTML(){
+    var html = "";
+
+    for (var i = 0; i < streamList.length; i++){
+        html +=
+            '<tr data-status="' + streamList[i].onlineStatus + '">' +
+                '<td>' +
+                    '<a href=' + streamList[i].url + '>' +
+                        '<div class="media">' +
+                            '<div class="pull-left">' +
+                                '<img src="' + streamList[i].logo + '" class="media-photo">' +
+                            '</div>' +
+                            '<div>' +
+                                '<h4 class="title">' +
+                                    streamList[i].name +
+                                    '<span class="pull-right ' + streamList[i].onlineStatus + '">' + streamList[i].onlineStatus + '</span>' +
+                                '</h4>' +
+                                '<p class="summary">' + streamList[i].description + '</p>' +
+                            '</div>' +
+                        '</div>' +
+                    '</a>' +
+                '</td>' +
+            '</tr>';
+    }
+
+    $("#stream-table-body").html(html);
+}
